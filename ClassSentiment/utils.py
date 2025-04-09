e9326ed94c38815b27a62b7ae455eb278eb32797
Dipa <mondaldipa0108@gmail.com>
2025-04-09 08:30:27 +0530

    Create utils.py

---
+++ ClassSentiment/utils.py
@@ -0,0 +1,10 @@
+def map_emotion_to_sentiment(label):
+    label = label.lower()
+    if label in ['confusion', 'sadness', 'fear']:
+        return 'Confused'
+    elif label in ['joy', 'love', 'surprise']:
+        return 'Engaged'
+    elif label in ['anger', 'disgust']:
+        return 'Bored'
+    else:
+        return 'Neutral'
